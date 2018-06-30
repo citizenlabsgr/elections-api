5fe94a7b5d6cdbbb89aabd948f3f0cb3cac8e3e8
Jace Browning <jacebrowning@gmail.com>
2018-06-30 15:04:59 -0400

    Render the README as the homepage

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Elections API
 
-[![CircleCI](https://circleci.com/gh/citizenlabsgr/elections-api.svg?style=svg)](https://circleci.com/gh/citizenlabsgr/elections-api)
+[![CircleCI](https://circleci.com/gh/citizenlabsgr/elections-api.svg?style=svg)](https://circleci.com/gh/citizenlabsgr/elections-api) <!-- skip -->
+
+APIs to check voter registration status and view upcoming ballots in Michigan.
+
+## Overview
+
+> TBD
+
+## Documentation
+
+Interactive API documentation powered by [Swagger UI](https://swagger.io/tools/swagger-ui/), can be found [here](/docs).
+
+## History
+
+**Version 0.0**
+
+- > TBD
--- config/urls.py
+++ config/urls.py
@@ -1,16 +1,28 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
+from pathlib import Path
 
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import include, path
+
+from markdown import markdown
 from rest_framework_swagger.views import get_swagger_view
 
 
-schema_view = get_swagger_view(title="Elections API")
+def readme(request):
+    path = Path(__file__).parent.parent / "README.md"
+    text = ""
+    with path.open() as f:
+        for line in f:
+            if "-- skip --" not in line:
+                text += line
+    html = markdown(text)
+    return HttpResponse(html)
+
 
 urlpatterns = [
     path("api/", include("elections.urls")),
     path("admin/", admin.site.urls),
     path("grappelli/", include("grappelli.urls")),
-    path("docs/", schema_view),
-    path("", RedirectView.as_view(url="/docs/")),
+    path("docs/", get_swagger_view(title="Elections API")),
+    path("", readme),
 ]
