616d929fac2cae90fb98b954cf86eb3f6da72ecb
Jace Browning <jacebrowning@gmail.com>
2018-07-01 22:24:53 -0400

    Try drf-yasg

--- config/urls.py
+++ config/urls.py
@@ -4,16 +4,26 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import TemplateView
 
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from elections import helpers
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Michigan Elections API",
+        default_version='0',
+        description="Voter registration status and ballots for Michigan.",
+    )
+)
 
 
 urlpatterns = [
     path('api/', include('elections.urls')),
     path('admin/', admin.site.urls),
     path('grappelli/', include('grappelli.urls')),
-    path('docs/', get_swagger_view(title="Michigan Elections API")),
+    path('docs/', schema_view.with_ui('swagger')),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
 
