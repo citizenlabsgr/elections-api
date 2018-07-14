ef183738338e2ef6ec427b769ef96c2bbfeac4ab
Jace Browning <jacebrowning@gmail.com>
2018-07-14 13:57:12 -0400

    Add logging

--- elections/management/commands/migrate_data.py
+++ elections/management/commands/migrate_data.py
@@ -3,7 +3,7 @@ from django.db.utils import IntegrityError
 
 import log
 
-from elections import models
+from elections import models  # pylint: unused-import
 
 
 class Command(BaseCommand):
@@ -12,12 +12,14 @@ class Command(BaseCommand):
     def handle(self, *_args, **_kwargs):
         log.init(reset=True)
         for precinct in models.Precinct.objects.filter(ward='0'):
+            log.warn(f'Fixing precinct: {precinct}')
             precinct.ward = ''
             try:
                 precinct.save()
             except IntegrityError:
                 precinct.delete()
         for precinct in models.Precinct.objects.filter(number='0'):
+            log.warn(f'Fixing precinct: {precinct}')
             precinct.number = ''
             try:
                 precinct.save()
