044d00c2ac1b7b774aa491b4c2012f782026dac2
Jace Browning <jacebrowning@gmail.com>
2018-10-02 23:30:13 -0400

    Order by precinct ID

--- elections/management/commands/clean_data.py
+++ elections/management/commands/clean_data.py
@@ -22,18 +22,22 @@ class Command(BaseCommand):
             raise e from None
 
     def run(self):
-        election = self.get_current_election()
+        election = Election.objects.filter(active=True).get()
+
         for ballot in Ballot.objects.filter(election=election):
-            websites = ballot.websites.all()
+
+            websites = ballot.websites.order_by('mi_sos_precinct_id')
             count = len(websites)
             log.debug(f'Ballot {ballot.id}: {count} website(s)')
 
             if count == 1:
                 website = websites[0]
+
                 if not website.source:
                     self.stdout.write(f'Set source of truth: {website}')
                     website.source = True
                     website.save()
+
                 if ballot.precinct.mi_sos_id != website.mi_sos_precinct_id:
                     self.stdout.write(f'Set precinct ID: {ballot.precinct}')
                     assert website.mi_sos_precinct_id
@@ -47,10 +51,3 @@ class Command(BaseCommand):
 
             else:
                 log.warn(f'Ballot has no websites: {ballot}')
-
-    def get_current_election(self) -> Election:
-        return (
-            Election.objects.filter(active=True)
-            .exclude(mi_sos_id=None)
-            .first()
-        )
