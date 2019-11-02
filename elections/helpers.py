fbfba64541f935d0f30bdf60e86941da0b453062
Santiago Quiroga <quirogas@mail.gvsu.edu>
2019-11-02 19:39:52 -0400

    still need to be 100% sure this will pass all cases

--- elections/helpers.py
+++ elections/helpers.py
@@ -394,3 +394,15 @@ def check_repeadted(text):
         for other_index in range(index + 1, len(list_of_words)):
             if(word == list_of_words[other_index]):
                 repeated_list.append((index,word),(other_index,list_of_words[other_index]))
+
+
+    initial_index = None
+    consecutive_count = 0
+    
+    for index, repeated_tuple in enumerate(repeated_list):
+        if(initial_index == None):
+            initial_index = (index,repeated_tupe[0][0])
+            count += 1
+        elif (len(repeated_list) > index + 1 and repeated[0][0] + 1 == repeated_list[index + 1][0][0]):
+            #TODO: check if the value of the next index would equals the value of the second pair for the tuple
+
