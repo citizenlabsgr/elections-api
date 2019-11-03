86b9d8b661e59709b40be8ece8cec49182d6eca9
Santiago Quiroga <quirogas@mail.gvsu.edu>
2019-11-03 12:12:35 -0500

    dun

--- text_check.py
+++ text_check.py
@@ -7,32 +7,40 @@ def check_repeated(text):
         if char not in '''!()-[]{};:'"\,<>./?@#$%^&*_~''':
             no_punct = no_punct + char
 
-    # display the unpunctuated string
-    print(no_punct)
 
     list_of_words = no_punct.split()
     repeated_list = []
 
+    # Finds all repeated words
     for index, word in enumerate(list_of_words):
         for other_index in range(index + 1, len(list_of_words)):
             if(word == list_of_words[other_index]):
                 repeated_list.append(((index, word), (other_index, list_of_words[other_index])))
+                break
 
 
-    return repeated_list
-
-if __name__ == "__main__":
-    print( check_repeated(sys.argv[1]))
-
-"""
-    initial_index = None
+    initial_tuple = None
     consecutive_count = 0
 
+    #checks if its trully repeated
     for index, repeated_tuple in enumerate(repeated_list):
-        if(initial_index == None):
-            initial_index = (index,repeated_tupe[0][0])
-            count += 1
-        elif (len(repeated_list) > index + 1 and repeated[0][0] + 1 == repeated_list[index + 1][0][0]):
-            #TODO: check if the value of the next index would equals the value of the second pair for the tuple
-"""
+        if(initial_tuple == None):
+            initial_tuple = repeated_tuple 
+            consecutive_count += 1
+        elif (len(repeated_list) > index + 1 and repeated_tuple[0][0] + 1 == repeated_list[index + 1][0][0]):
+            consecutive_count += 1
+        elif(consecutive_count> 0 and repeated_tuple[0][0] - 1 == repeated_list[index - 1][0][0]): 
+            consecutive_count += 1
+        elif(initial_tuple[0][0] + consecutive_count == initial_tuple[1][0]):
+            print("repeated sentece")
+            for index in range(initial_tuple[0][0], initial_tuple[0][0] + consecutive_count + 1):
+                print(list_of_words[index])
+        else:
+            print(repeated_tuple, "nothing here")
+            consecutive_count = 0
+            initial_tuple = None
+
+    return repeated_list
+if __name__ == "__main__":
+    print( check_repeated(sys.argv[1]))
 
