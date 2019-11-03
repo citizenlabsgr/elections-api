8225691b6ba17a3b7978eddd8ee85b3d7e7ee0cb
Santiago Quiroga <quirogas@mail.gvsu.edu>
2019-11-03 13:34:04 -0500

    did the linting

--- elections/helpers.py
+++ elections/helpers.py
@@ -341,7 +341,7 @@ def parse_proposals(ballot: BeautifulSoup, data: Dict) -> int:
         ),
         start=1,
     ):
-        
+
         log.debug(f'Parsing proposal item {index}: {item}')
 
         if "section" in item['class']:
@@ -382,8 +382,8 @@ def parse_proposals(ballot: BeautifulSoup, data: Dict) -> int:
             proposal['text'] = label
             count += 1
 
-
     return count
+
 
 def check_repeated(text):
     # remove punctuation from the string
@@ -392,42 +392,47 @@ def check_repeated(text):
         if char not in '''!()-[]{};:'"\,<>./?@#$%^&*_~''':
             no_punct = no_punct + char
 
-
     list_of_words = no_punct.split()
     repeated_list = []
 
     # Finds all repeated words
     for index, word in enumerate(list_of_words):
         for other_index in range(index + 1, len(list_of_words)):
-            if(word == list_of_words[other_index]):
-                repeated_list.append(((index, word), (other_index, list_of_words[other_index])))
+            if word == list_of_words[other_index]:
+                repeated_list.append(
+                    ((index, word), (other_index, list_of_words[other_index]))
+                )
                 break
-
 
     initial_tuple = None
     consecutive_count = 0
 
-    #checks if its trully repeated
+    # checks if its trully repeated
     for index, repeated_tuple in enumerate(repeated_list):
-        if(initial_tuple == None):
+        if initial_tuple is None:
             initial_tuple = repeated_tuple
             consecutive_count += 1
-            if (repeated_tuple[0][0] + 1 == repeated_tuple[1][0]):
+            if repeated_tuple[0][0] + 1 == repeated_tuple[1][0]:
                 bugsnag.notify(ValueError("duplicate value found"))
                 initial_tuple = None
                 consecutive_count = 0
-        elif (len(repeated_list) > index + 1 and repeated_tuple[0][0] + 1 == repeated_list[index + 1][0][0]):
+        elif (
+            len(repeated_list) > index + 1
+            and repeated_tuple[0][0] + 1 == repeated_list[index + 1][0][0]
+        ):
             consecutive_count += 1
-        elif(consecutive_count> 0 and repeated_tuple[0][0] - 1 == repeated_list[index - 1][0][0]):
+        elif (
+            consecutive_count > 0
+            and repeated_tuple[0][0] - 1 == repeated_list[index - 1][0][0]
+        ):
             consecutive_count += 1
-        elif(initial_tuple[0][0] + consecutive_count == initial_tuple[1][0]):
-            #TODO: notify there was a duplicate found
+        elif initial_tuple[0][0] + consecutive_count == initial_tuple[1][0]:
+            # TODO: notify there was a duplicate found
             bugsnag.notify(ValueError("duplicate value found"))
-            for index in range(initial_tuple[0][0], initial_tuple[0][0] + consecutive_count + 1):
+            for index in range(
+                initial_tuple[0][0], initial_tuple[0][0] + consecutive_count + 1
+            ):
                 print(list_of_words[index])
         else:
             consecutive_count = 0
             initial_tuple = None
-
-
-
