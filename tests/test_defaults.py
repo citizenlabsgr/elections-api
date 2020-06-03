795842f6e657c9ac938dfcea400148dc584008cc
Jace Browning <jace.browning@zapier.com>
2020-06-02 23:15:54 -0400

    Attach county delegates to precincts

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -18,5 +18,5 @@ def describe_initialize_districts():
         defaults.initialize_districts()
         defaults.initialize_districts()
 
-        expect(DistrictCategory.objects.count()) == 25
+        expect(DistrictCategory.objects.count()) == 26
         expect(District.objects.count()) == 1
