5b54c79ae272cdff0a9b4c615fbc96502e1d4df8
Jace Browning <jacebrowning@gmail.com>
2020-08-28 18:29:10 -0400

    Set hour of scraper version

--- elections/constants.py
+++ elections/constants.py
@@ -5,5 +5,5 @@ from pendulum import datetime
 
 MI_SOS_URL = "https://mvic.sos.state.mi.us"
 
-SCRAPER_LAST_UPDATED = datetime(2020, 8, 28, tz=settings.TIME_ZONE)
+SCRAPER_LAST_UPDATED = datetime(2020, 8, 28, hour=19, tz=settings.TIME_ZONE)
 PARSER_LAST_UPDATED = datetime(2020, 8, 23, tz=settings.TIME_ZONE)
