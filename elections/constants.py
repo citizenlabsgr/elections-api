130708826152b2c87788e0d245d3ec5fecbedaa0
Jace Browning <jacebrowning@gmail.com>
2023-06-24 16:52:52 -0400

    Update constants.py

--- elections/constants.py
+++ elections/constants.py
@@ -9,5 +9,5 @@ TERMS = {
     "Representative in State Legislature": "2 Year Term",
 }
 
-SCRAPER_LAST_UPDATED = datetime(2022, 11, 9, tz=settings.TIME_ZONE)
+SCRAPER_LAST_UPDATED = datetime(2023, 6, 25, tz=settings.TIME_ZONE)
 PARSER_LAST_UPDATED = datetime(2023, 3, 14, tz=settings.TIME_ZONE)
